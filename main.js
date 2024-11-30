96e9153199a2a6dcbefa8425f5e07bb353462043
Augusto-logo <augusto2.logo@gmail.com>
2024-11-30 20:53:48 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-// import { LerDados } from './teste';
 
+// Express para ligar o servidor web
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let cors = require("cors");
+app.use(cors());
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Módulos para manipulação dos arquivos e pastas
+const fs = require('fs');
+const path = require('path');
+
+// Modulos relacionados ao whatsapp-web.js
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 // Função para mandar MediaType(PDF, foto, etc)
@@ -33,26 +48,48 @@ client.on('message_create', message => {
 });
 
 //Função para retorno de texto
-client.on('message_create', message => {
-	if (message.body.toLowerCase() === 'boletim') {
-        text = "Informe seu telefone e nome do aluno!"
-		// send back "pong" to the chat the message was sent in
-		client.sendMessage(message.from, text);
-	}
-});
+// client.on('message_create', message => {
+// 	if (message.body.toLowerCase() === 'boletim') {
+//         text = "Informe seu telefone e nome do aluno!"
+// 		// send back "pong" to the chat the message was sent in
+// 		client.sendMessage(message.from, text);
+// 	}
+// });
 
-// Função pra retorno de imagem
-client.on('message', async (msg) => {
-    if (msg.body === 'media') {
-        const media = new MessageMedia('image.png', base64Image);
-        await client.sendMessage(msg.from, media);
+
+// Função para retorno de PDF a partir de uma mensagem no WPP
+// client.on('message', async (msg) => {
+//     if (msg.body === 'pdf') {
+//         const media = MessageMedia.fromFilePath('Assets/BoletimSingular.pdf');
+//         await client.sendMessage(msg.from, media);
+//     }
+// });
+
+function VerificaArquivo(path, arquivo){
+    const arquivos = fs.readdirSync(path);
+    return arquivos.includes(arquivo);
+}
+
+// Rota para receber dados via POST
+app.post('/receber-dados', async (req, res) => {
+    // Recebe os dados enviados
+    const data = req.body;
+    console.log('Dados recebidos:', data);
+    res.status(200).send('Dados recebidos com sucesso');
+
+    // Verifica se o arquivo existe
+    const nomeArquivo = data.nome + '_' + data.turma + '.pdf';
+    const diretorio = 'Assets/Turmas/' + data.turma + '/';
+
+    if (VerificaArquivo(diretorio, nomeArquivo)){
+        // msg = '55' + data.telefone + '@c.us'
+        msg = '553398605771@c.us'
+        const media = MessageMedia.fromFilePath(path.join(diretorio,nomeArquivo));
+        await client.sendMessage(msg, media);
     }
 });
 
-// Função para retorno de PDF
-client.on('message', async (msg) => {
-    if (msg.body === 'pdf') {
-        const media = new MessageMedia('Assets/BoletimSingular.pdf', base64PDF, 'PDF');
-        await client.sendMessage(msg.from, media);
-    }
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`);
 });
--- server.js
+++ server.js
@@ -5,17 +5,48 @@ const port = 3000;
 let cors = require("cors");
 app.use(cors());
 
+const fs = require('fs');
+const path = require('path');
+
+
 // Middleware para parsear JSON
 app.use(express.json());
 
+
+
+function VerificaArquivo(path, arquivo){
+    const arquivos = fs.readdirSync(path);
+    return arquivos.includes(arquivo);
+}
+
 // Rota para receber dados via POST
-app.post('/receber-dados', (req, res) => {
+app.post('/receber-dados', async (req, res) => {
+    // Recebe os dados enviados
     const data = req.body;
     console.log('Dados recebidos:', data);
     res.status(200).send('Dados recebidos com sucesso');
+
+    // Verifica se o arquivo existe
+    const nomeArquivo = data.nome + '_' + data.turma + '.pdf';
+    const diretorio = 'Assets/Turmas/' + data.turma + '/';
+
+    console.log('Diretório:', diretorio);
+    console.log('Nome do arquivo:', nomeArquivo);
+
+    if (VerificaArquivo(diretorio, nomeArquivo)){
+        // msg = '55' + data.telefone + '@c.us'
+        msg = '553398605771@c.us'
+        console.log('Enviando arquivo para:', msg);
+    } else{
+        console.log('Arquivo não encontrado');
+    }
 });
+
+
+
 
 // Inicia o servidor
 app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`);
 });
+
