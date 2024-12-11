e951e5a7e4e4d0d7155c1d297073c59b8a448853
Augusto-logo <augusto2.logo@gmail.com>
2024-12-11 11:23:05 -0300

    organização
    Organizei o arquivo teste, falta terminar o main.js

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-// Express para ligar o servidor web
+// Importação do express e configurações básicas para ligar o servidor
 const express = require('express');
 const multer = require('multer');
 let cors = require("cors");
 
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
 
 // Módulos para manipulação dos arquivos e pastas
 const fs = require('fs');
 const path = require('path');
 
-const app = express();
-const port = 3000;
-
-// Middleware para parsear JSON
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-
-// Função para verificar e criar pasta se não existir
+// **Funções
+// Função para verificar se o diretório existe e, caso não exista, criar ele e as subpastas
 const verificaDiretorio = (diretorio) => {
     if (!fs.existsSync(diretorio)) {
-      fs.mkdirSync(diretorio, { recursive: true }); // Cria a pasta e subpastas, se necessário
+      fs.mkdirSync(diretorio, { recursive: true }); // Recursive cria a pasta e subpastas, se necessário.
     }
   };
-
-// Configuração do Multer para salvar arquivos no diretório "uploads"
+// Função para configurar os dados para o multer salvar os arquivos.
+// path(upload/ano/trimestre/nomeDaTurma)
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const ano = new Date().getFullYear().toString();// Ano atual
+        const ano = new Date().getFullYear().toString(); // Ano atual
         const trimestre = Math.floor((new Date().getMonth() + 3) / 3).toString(); // Trimestre atual
 
         let folderName = req.body.folderName; // Nome da subpasta vindo do corpo da requisição
@@ -46,7 +45,7 @@ const storage = multer.diskStorage({
     
 const upload = multer({ storage });
 
-// Middleware para processar o upload de múltiplos arquivos
+// Não está sendo utilizada(REMOVER)
 const processarUpload = (req, res) => {
     const uploadMultiple = upload.array('files');
     uploadMultiple(req, res, (err) => {
@@ -56,15 +55,45 @@ const processarUpload = (req, res) => {
     });
 };
 
-// Rota para upload de arquivos
+// Função para achar o diretório
+// A função verifica se cada etapa do path é uma diretório existente, gerando uma lista do path fornecido
+function lerPastas(diretorio) {
+    try {
+        const subpastas = fs.readdirSync(diretorio, { withFileTypes: true })
+            .filter(dirent => dirent.isDirectory())
+            .map(dirent => dirent.name);
+        return subpastas;
+    } catch (err) {
+        console.error('Erro ao ler diretório:', err);
+        return [];
+    }
+}
+
+// Função para achar o arquivo
+// A função verifica se cada etapa do path é um arquivo existente, gerando uma lista do path fornecido
+function lerArquivos(diretorio) {
+    try {
+        const arquivos = fs.readdirSync(diretorio, { withFileTypes: true })
+            .filter(dirent => dirent.isFile())
+            .map(dirent => dirent.name);
+        return arquivos;
+    } catch (err) {
+        console.error('Erro ao ler arquivos:', err);
+        return [];
+    }
+}
+
+// **ROTAS
+// ROTAS PARA UPLOAD DOS ARQUIVOS
+
+// Salva um arquivo 
 app.post('/upload', upload.single('file'), (req, res) => {
+    
     const ano = new Date().getFullYear();
     res.json({ message: 'Upload realizado com sucesso', filePath: `/uploads/${ano}/4 º trimestre/${req.file.filename}` });
 });
-
 // Servindo a pasta "uploads" de forma estática para acessar os arquivos enviados
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 // Rota para receber dados via POST
 app.post('/receber-dados', async (req, res) => {
     // Recebe os dados enviados
@@ -82,34 +111,6 @@ app.post('/receber-dados', async (req, res) => {
         res.status(400).send('Nenhum arquivo recebido');
     }
 });
-
-
-// FUnção para achar o diretório de pastas
-function lerPastas(diretorio) {
-    try {
-        const subpastas = fs.readdirSync(diretorio, { withFileTypes: true })
-            .filter(dirent => dirent.isDirectory())
-            .map(dirent => dirent.name);
-        return subpastas;
-    } catch (err) {
-        console.error('Erro ao ler diretório:', err);
-        return [];
-    }
-}
-
-// Função que retorna os arquivos de um diretório
-function lerArquivos(diretorio) {
-    try {
-        const arquivos = fs.readdirSync(diretorio, { withFileTypes: true })
-            .filter(dirent => dirent.isFile())
-            .map(dirent => dirent.name);
-        return arquivos;
-    } catch (err) {
-        console.error('Erro ao ler arquivos:', err);
-        return [];
-    }
-}
-
 
 // Rota para enviar dados do formulário
 // Dados para o preenchimento da turma e dos nomes
@@ -130,7 +131,7 @@ app.get('/carrega-nomes', async (req, res) => {
     console.log(nomes); // Saída: ['P1', 'P2', 'P3'] ou ['P4', 'P5', 'P6']
 });
 
-// Inicia o servidor
+// Start do servidor
 app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`);
 });
